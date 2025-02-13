0d6d24db97335902977c9bc288a4ba948caa9595
Rendekendo <maxikrendo@gmail.com>
2025-02-13 20:53:01 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header.jsx'
+import Gamba from './Gamba.jsx'
+
+function App() {
+
+  return(
+    <div id="appDiv">
+      <Header/>
+      <Gamba/>
+    </div>
+  );
+}
+
+export default App
---
+++ src/Gamba.jsx
@@ -0,0 +1,50 @@
+import {useState} from "react";
+
+function Gamba(){
+    const [money, setMoney] = useState(0);
+    const [gamba, setGamba] = useState(0);
+    const [gambaDisplay, setGambaDisplay] = useState("Spin to Win");
+
+    function gamble() {
+        document.getElementById("loseStatus").style.color = "whitesmoke";
+        document.getElementById("winStatus").style.color = "whitesmoke";
+        document.getElementById("win2Status").style.color = "whitesmoke";
+        if (money == 0) {
+            setGambaDisplay("You can't gemble without any money")
+        }
+        else {
+            const gambaValue = (Math.random()*10);
+            setGamba(gambaValue);
+
+            if (gambaValue > 0 && gambaValue < 4){
+                setMoney(0);
+                setGambaDisplay("You Lose EVERYTHING")
+                document.getElementById("loseStatus").style.color = "yellow";
+            }
+            else if (gambaValue > 4 && gambaValue < 8) {
+                setMoney(m => m * 1.5);
+                setGambaDisplay("You Win (x1.5)")
+                document.getElementById("winStatus").style.color = "yellow";
+            }
+            else if (gambaValue > 8 && gambaValue < 10) {
+                setMoney(m => m * 3);
+                setGambaDisplay("You Win BIG (x3)")
+                document.getElementById("win2Status").style.color = "yellow";
+            }
+        }
+    }
+
+    return(
+        <div id="gambaDiv">
+            <h1>Gamba</h1>
+            <button className="earnButton" onClick={() => setMoney(m => m +0.1)}>Click Here</button>
+            <p>€{money.toFixed(1)}</p>
+            <button className="gambaButton" onClick={gamble}>Roll Dice</button>
+            <p><span id="loseStatus">0 - 4 Lose</span>,<span id="winStatus"> 4 - 8 Win</span>,<span id="win2Status"> 8 - 10 Win Big</span></p>
+            <p>Last Rolled Number: {Math.round(gamba)}</p>
+            <p>{gambaDisplay}</p>
+        </div>
+    );
+}
+
+export default Gamba
